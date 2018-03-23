e02057feed9552a74b83d2a433681c55c22c151c
Pherang <pherang@users.noreply.github.com>
2018-03-22 20:21:11 -0400

    Timestamp service now working

--- timestamp.js
+++ timestamp.js
@@ -1,5 +1,50 @@
-var express = require('express'),
-    app     = express()
+const http = require('http')
+
+// Simple service that returns an object that contains the unix timestamp
+// and a natural language date. 
+// Decided to code this using http only and not express
+http.createServer( (req,res) => {
+
+  const { headers, method, url } =  req
+
+  req.on('error', (err) => {
+   console.log(err)
+  });
+
+  res.on('error', (err) => {
+    console.log(err)
+  });
+
+  if (req.method === 'GET' && req.url === '/') {
+    
+    let instructions = 'Enter a date in natural language or milliseconds in the url'
+    instructions += ' \nE.g. http://hostname/January 1, 2000'
+    instructions += ' \nE.g. http://hostname/12378321678321'
 
 
+    res.end(instructions)
+    console.log('No date specfied in URL')
+  }
+  if (req.method === 'GET' && req.url.length >= 2) {
 
+    let timeInput = req.url.substring(1).replace(/%20/g, " ")
+    let timeInteger = Number.parseInt(timeInput, 10)
+    if (Number.isInteger(timeInteger)) {
+      console.log('Is Integer!')
+      timeInput = timeInteger
+    }
+    let timeObject = {} 
+    let unixTime = new Date(timeInput)
+    console.log(timeInput)
+    console.log(unixTime)
+    if (!(Number.isNaN(unixTime))) {
+      timeObject.unix = unixTime.getTime()
+      timeObject.natural = unixTime.toDateString()
+      res.end(JSON.stringify(timeObject))
+    } else {
+      res.end('Date format invalid')
+    }
+  }
+
+}).listen(8080);
+console.log('Server listening on 8080')
